f23e6fe27e0d4ed23c500e59cc7301bc2443b653
Gustavo Avila <gusavila92@gmail.com>
2022-08-24 20:25:41 -0500

    Add authenticate_user action

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,22 @@
 class ApplicationController < ActionController::API
+  before_action :authenticate_user
+
+  helper_method :current_user
+  attr_reader :current_user
+
+  private
+  
+  def authenticate_user
+    header = request.headers.fetch('Authorization', '').split(' ')
+    if header[0] == 'Bearer'
+      payload = JWT.decode(token, nil, false)[0](header[1])
+      id = payload['sub']
+      @current_user = User.find_by_id!(id)
+    else
+      render json: { status: 'unauthorized' }, status: :unauthorized
+    end
+  rescue JWT::DecodeError, ActiveRecord::RecordNotFound
+    render json: { status: 'unauthorized' }, status: :unauthorized
+  end
+
 end
